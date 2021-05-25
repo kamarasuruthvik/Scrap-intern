89fbd1144a75bc2504ac05608f486dd43a9ecb32
arthik444 <58880788+arthik444@users.noreply.github.com>
2021-05-25 16:51:03 +0530

    javascript for UI

--- excel.js
+++ excel.js
@@ -4,11 +4,7 @@ document.getElementById('input').addEventListener("change", (event) => {
     selectedFile = event.target.files[0];
 })
 
-let data=[{
-    "name":"jayanth",
-    "data":"scd",
-    "abc":"sdef"
-}]
+let data=[];
 
 
 document.getElementById('button').addEventListener("click", () => {
@@ -23,8 +19,51 @@ document.getElementById('button').addEventListener("click", () => {
          workbook.SheetNames.forEach(sheet => {
               let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
               console.log(rowObject);
-              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
+              var error= validateObject(rowObject);
+              if(error==='Success!'){
+               makeTable(rowObject);
+              }
+            //   document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
          });
         }
     }
 });
+function validateObject(info){
+    var errorMessage=document.getElementById('error-container');
+    var currentError=null;
+    console.log(info);
+    try{
+        if(info[0]['linkedIn page link']===undefined || info[0]['Company Name']===undefined)
+            throw "Please enter the fields correctly. If facing difficulty then use the sample template provided above and fill it!";
+        else
+            throw "Success!";
+    }
+    catch(error){
+        errorMessage.innerHTML=error;
+        currentError= error;
+    }
+    return currentError;
+}
+function makeTable(info){
+    myTable=document.getElementById('myTable');
+    inter=document.getElementById('inner')
+    console.log(info);
+    inter.innerHTML=`<tr>
+    <th scope="col">#</th>
+    <th scope="col">Company Name</th>
+    <th scope="col">Linkedin link</th>
+  </tr>`
+    for(let i=0;i<info.length;i++){
+        myTable.innerHTML+=`<tr>
+        <th scope="row">${i+1}</th>
+        <td>
+        ${info[i]['Company Name']}
+        </td>
+        <td><a href="${info[i]['linkedIn page link']}">
+        ${info[i]['linkedIn page link']}
+        <a>
+        </td>
+        </tr>`
+        console.log(info[i]['Company Name']);
+    }
+}
